f13886b7e96da933ca1bb6ff65c2f0cb581a1f23
Michael McGee <dineromike@yahoo.com>
2022-08-23 12:33:42 -0500

    refactored some code

--- src/app/verb-domain/verb-list/verb-list.component.ts
+++ src/app/verb-domain/verb-list/verb-list.component.ts
@@ -17,7 +17,7 @@ export class VerbListComponent implements OnInit {
   showTranslation: boolean = false;
 
   presentTense!: Tense | null;
-  pastTense!: Tense | null;
+  preteriteTense!: Tense | null;
   sentence!: FromTo | null;
   showSentence: boolean = false;
   sentenceList: FromTo[] = [];
@@ -50,28 +50,28 @@ export class VerbListComponent implements OnInit {
 
         //baseline
         this.verb = verb;
+        this.presentTense = null;
         this.showTranslation = false;
 
         this.presentTense = this.conjugationService.getPresentTense(verb);
         //TOOO fix this so it uses the a non string version
-        this.pastTense = this.conjugationService.getPastTense(this.verb.to);
-        
-        this.sentence = null;        
+        this.preteriteTense = this.conjugationService.getPastTense(this.verb.to);
+
+        this.sentence = null;
         if (verb.presentTense && verb.presentTense.fistPersonSingular && verb.presentTense.fistPersonSingular.sentenceList) {
           if (verb.presentTense.fistPersonSingular.sentenceList.length > 0) {
             this.sentence = verb.presentTense.fistPersonSingular.sentenceList[0];
             this.sentenceList = verb.presentTense.fistPersonSingular.sentenceList;
           }
         }
-        
+
         //show the translation
         await wait(waitTime);
         this.showTranslation = true;
 
         //Wait to rotate to next verb
         await wait(waitTime + 8000);
-        
-        this.presentTense = null;
+
       }
     };
     loop();
