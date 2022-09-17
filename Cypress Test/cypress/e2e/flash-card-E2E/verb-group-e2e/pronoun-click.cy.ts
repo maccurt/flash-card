eb02872d73afdad505d6be85bf7ac037af5fbb65
Michael McGee <dineromike@yahoo.com>
2022-09-17 07:34:19 -0500

    added the most basic cypress test for pronoun click

--- Cypress Test/cypress/e2e/flash-card-E2E/verb-group-e2e/pronoun-click.cy.ts
+++ Cypress Test/cypress/e2e/flash-card-E2E/verb-group-e2e/pronoun-click.cy.ts
@@ -4,13 +4,38 @@ describe('Name of the group', () => {
         cy.visit('/verb-group/1');
     })
 
-    describe('Name of the group', () => {
+    describe('clicking each pronoun should have sentence-0, AKA atleast one sentence', () => {
 
-        it('first person singular option should exist and have value of 1', () => {
+        //1st
+        it('first person singular', () => {
+            cy.getDataTestId('present-tense').getDataTestId('fps-radio').click();
+            cy.getDataTestId('sentence-0').should('exist');            
+        });
+        it('first person plurual', () => {
             cy.getDataTestId('present-tense').getDataTestId('fpp-radio').click();
-                
+            cy.getDataTestId('sentence-0').should('exist');            
         });
 
-    });
+        //2nd
+        it('2nd person singular', () => {
+            cy.getDataTestId('present-tense').getDataTestId('sps-radio').click();
+            cy.getDataTestId('sentence-0').should('exist');            
+        });
+        it('2nd person plural', () => {
+            cy.getDataTestId('present-tense').getDataTestId('spp-radio').click();
+            cy.getDataTestId('sentence-0').should('exist');            
+        });
 
+
+        //3rd
+        it('third person plural', () => {
+            cy.getDataTestId('present-tense').getDataTestId('tps-radio').click();
+            cy.getDataTestId('sentence-0').should('exist');            
+        });
+
+        it('third person singular', () => {
+            cy.getDataTestId('present-tense').getDataTestId('tpp-radio').click();
+            cy.getDataTestId('sentence-0').should('exist');            
+        });        
+    });
 });
